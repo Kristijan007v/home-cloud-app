f7f41cce51915197828ede4092da322d064f0295
Kristijan007v <kiki.vidovic.6969@gmail.com>
2022-02-07 04:46:03 +0100

    Fixed bugs

--- app/main.py
+++ app/main.py
@@ -1,14 +1,11 @@
 from http.cookiejar import MozillaCookieJar
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, request, jsonify, flash
 from flask_login import login_required, current_user
 from . import db
 import os
+from werkzeug.utils import secure_filename
 
 main = Blueprint('main', __name__)
-
-@main.route('/')
-def index():
-    return render_template('index.html')
 
 """ @main.route('/sendmail')
 @login_required
@@ -21,9 +18,9 @@ def sendmail():
 def profile():
     return render_template('profile.html', name=current_user.name, email=current_user.email)
 
-@main.route('/my-cloud')
+@main.route('/')
 @login_required
-def load_cloud():
+def index():
 
     email = session['email']
 
@@ -49,6 +46,40 @@ def load_cloud():
             temp_images = os.path.join(path + '/', image)
             image_names_list.append(image)
             images_list.append(temp_images)
-    return render_template('my-cloud.html', files=file_list, filenames = filename_list, hists = images_list)
+    return render_template('index.html', files=zip(file_list, filename_list), hists = zip(images_list, image_names_list))
 
+#Allowed file types for upload
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+
+
+#File upload API route
+@main.route('/file-upload', methods=['POST'])
+def upload_file():
+	# check if the post request has the file part
+	if 'file' not in request.files:
+		resp = jsonify({'message' : 'No file part in the request'})
+		resp.status_code = 400
+		return resp
+	file = request.files['file']
+	#check if there is file selected for upload
+	if file.filename == '':
+		resp = jsonify({'message' : 'No file selected for uploading'})
+		resp.status_code = 400
+		return resp
+	#upload file to the server
+	if file:
+		filename = secure_filename(file.filename)
+		file.save(os.path.join(f"static/Cloud/vidovic@kristijan.me/images", filename))
+		resp = jsonify({'message' : 'File successfully uploaded'})
+		resp.status_code = 201
+		return resp
+	#Handle unallowed file types
+	else:
+		resp = jsonify({'message' : 'Allowed file types are txt, pdf, png, jpg, jpeg, gif'})
+		resp.status_code = 400
+		return resp
 
